9dfade9a1f325de66e82b7e1882cc287506262c8
Egor <kolyadintsev1997@mail.ru>
2020-08-03 12:27:04 +0500

    Закончил с версткой

---
+++ js/main.js
@@ -0,0 +1,22 @@
+let menuBtn = document.querySelector('.nav-main__hamburger')
+let modalBtn = document.querySelector('.contacts__modal')
+let closeBtn = document.querySelector('.modal__cross')
+let modal = document.querySelector('.modal')
+
+menuBtn.addEventListener('click', function() {
+    let menu = document.querySelector('.nav__list')
+    menu.classList.toggle('show')
+    menu.classList.toggle('hide')
+    menuBtn.classList.toggle('close')
+    console.log('1')
+})
+
+modalBtn.addEventListener('click', function() {
+    event.preventDefault()
+    modal.classList.toggle('show')
+})
+
+closeBtn.addEventListener('click', function() {
+    event.preventDefault()
+    modal.classList.toggle('show')
+})
